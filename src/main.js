d12c9a6b34a7ae234ae6c3ebf8952851234ac8d7
kikuchan612 <1040334581@qq.com>
2020-08-02 20:44:41 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '../public/css/reset.css'
+import '../public/css/border.css'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
